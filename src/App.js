b3ed17e14208103af0994b3907f57b962fedb0cf
takwa melki <takwamelki484@gmail.com>
2021-05-01 10:17:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-form">
+      <h1 className="text-center">Login Form</h1>
+      <Container>
+        <Row className="justify-content-md-center" md={3}>
+         <Col>    
+      <Form className="form-container">
+  <Form.Group controlId="formBasicEmail">
+    
+    <Form.Control type="email" placeholder="Enter email" />
+    
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out " />
+  </Form.Group>
+  <Button variant="primary" className="btn btn-primary btn-block" type="submit">
+    Log in 
+  </Button>
+</Form>
+</Col>
+</Row>
+</Container>
+
+      
     </div>
   );
 }
